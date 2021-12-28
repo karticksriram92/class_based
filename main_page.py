933d870e1cb894e19eef0898617378bbf5e9b17f
karticksriram92 <sriram5130@gmail.com>
2021-12-28 19:48:19 +0530

    modified and made display_img class, next mainpage

--- main_page.py
+++ main_page.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import ttk
 import doctors_page
 import layout
 
@@ -27,9 +28,11 @@ class main_page_display(main_page_layout):
     def __init__(self):
         #it will call parent class again so it will make another window, so either call this class at bottom(can use super() then) or dont call super()
         super().__init__()
-        self.show_img()
+        #self.show_img()
         button= Button(self.center_mainpage_frame , command=self.change_page)
         button.pack()
+        display_image(self.side_mainpage_frame,self.swidth, self.sheight)
+        
         #print(self.layout.swidth)
         
     # def get_img(self):
@@ -49,9 +52,9 @@ class main_page_display(main_page_layout):
             widgets.destroy()
         for widgets in self.side_mainpage_frame.winfo_children():
             widgets.destroy()
-        doctors_page.show_top_frame(self,self.top_mainpage_frame)
-        doctors_page.show_side_frame(self,self.side_mainpage_frame)
-        doctors_page.show_center_frame(self,self.center_mainpage_frame)
+        doctors_page.show_top_frame(self,self.top_mainpage_frame, self.swidth, self.sheight)
+        doctors_page.show_side_frame(self,self.side_mainpage_frame, self.swidth, self.sheight)
+        doctors_page.show_center_frame(self,self.center_mainpage_frame, self.swidth, self.sheight)
         
         
     def run(self):
@@ -59,21 +62,34 @@ class main_page_display(main_page_layout):
         
         
 class display_image(ttk.Label):
-    def __init__(self, container, ):
-    
+    def __init__(self, container, swidth, sheight):
+        #this is important using super() otherwise show_img wont work
+        super().__init__()
+        self.swidth = swidth
+        self.sheight = sheight
+        #print(self.sheight)
+        self.show_img(container)
     
     def get_img(self):
         img_file=Image.open("C:/Users/kartick/Downloads/test.png")
         size_fit=(int((self.swidth/4)-8), int(self.sheight-93))
         resized_img_file=img_file.resize(size_fit ,Image.ANTIALIAS)
         self.img=ImageTk.PhotoImage(resized_img_file)
+        #print(self.img)
         return self.img
     
-    def show_img(self):
+    #def show_img(self):
+    def show_img(self, container):
+        print(container)
         img = self.get_img()
-        label=Label(self.side_mainpage_frame,image=self.img)
-        button= Button(self.center_mainpage_frame , command=self.change_page)
-        button.pack()
+        #img = self.get_img()
+        #print(img)
+        print(self.img)
+        #label=Label(self.side_mainpage_frame,image=self.img)
+        label=Label(container,image=self.img)
+        #label=Label(container,text="hello world", bg='red')
+        # button= Button(self.center_mainpage_frame , command=self.change_page)
+        # button.pack()
         label.pack(fill=BOTH, expand="True")
 
 if __name__ == "__main__":
