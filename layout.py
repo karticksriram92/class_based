535dbda86285bc0130b6e477ad5cf04ebdc3caa1
karticksriram <sriram5130@gmail.com>
2021-12-29 11:12:03 +0530

    working right now

--- layout.py
+++ layout.py
@@ -13,47 +13,64 @@ class basic_layout(tk.Tk):
         self.state('zoomed')
         self.resizable(0,0)
 
-        self.swidth=self.winfo_screenwidth()
-        self.sheight=self.winfo_screenheight()
-        
-        
-        width=self.winfo_screenwidth()
-        height=self.winfo_screenheight()
+        self.rwidth=self.winfo_screenwidth()
+        self.rheight=self.winfo_screenheight()
         
         #top and main
         self.main_root_frame=Frame(self,bg='green')
         self.top_root_frame=Frame(self,bg='white')
         
         #side and center in main
-        self.side_frame=Frame(self.main_root_frame, bg = 'yellow',width=10, height=100)
-        self.center_frame=Frame(self.main_root_frame, bg = 'pink',width=10, height=100)
+        self.side_frame=Frame(self.main_root_frame, bg = 'yellow')
+        self.center_frame=Frame(self.main_root_frame, bg = 'pink')
         
         #top and main placing
-        self.top_root_frame.place(x=10,y=0,width=self.swidth-20,height=50,anchor='nw')
-        self.main_root_frame.place(x=10,y=50,width=self.swidth-20,height=self.sheight-85,anchor='nw')
+        self.top_root_frame.place(x=10,y=0,width=self.rwidth-20,height=50,anchor='nw')
+        self.main_root_frame.place(x=10,y=50,width=self.rwidth-20,height=self.rheight-85,anchor='nw')
         
         #side and center placing in main
         self.side_frame.grid(row=0, column=0, sticky='nsew')
         self.center_frame.grid(row=0, column=1, sticky='nsew')
         
-        
         self.main_root_frame.grid_columnconfigure(0, weight=1)
         self.main_root_frame.grid_columnconfigure(1, weight=4)
         self.main_root_frame.grid_rowconfigure(0, weight=1)
         self.main_root_frame.grid_rowconfigure(0, weight=1)
+        
+        self.update()
+        
+        self.top_root_width = self.top_root_frame.winfo_width()
+        self.top_root_height = self.top_root_frame.winfo_height()
+        
+        self.side_frame_width = self.side_frame.winfo_width()
+        self.side_frame_height = self.side_frame.winfo_height()
+        
+        self.center_frame_width = self.center_frame.winfo_width()
+        self.center_frame_height = self.center_frame.winfo_height()
 
 
 class main_layout(basic_layout):
     def __init__(self):
         super().__init__()
-        self.top_overlay_frame=Frame(self.top_root_frame, bg='black')
-        self.side_overlay_frame=Frame(self.side_frame, bg='white')
-        self.center_overlay_frame=Frame(self.center_frame, bg='grey')
+        
+        self.top_root_width = self.top_root_frame.winfo_screenwidth()
+        self.top_root_height = self.top_root_frame.winfo_screenheight()
+        
+        self.top_overlay_frame=Frame(self.top_root_frame, bg='black', width=self.top_root_width, height=self.top_root_height)
+        self.side_overlay_frame=Frame(self.side_frame, bg='white', width=self.side_frame_width, height=self.side_frame_height)
+        self.center_overlay_frame=Frame(self.center_frame, bg='grey', width=self.center_frame_width, height=self.center_frame_height)
                 
-        self.side_overlay_frame.pack(fill=BOTH, expand="True")
+        self.side_overlay_frame.place(x=0,y=0)
+        self.top_overlay_frame.place(x=0,y=0)
+        self.center_overlay_frame.place(x=0,y=0)
         
-        #prevent 
-        #self.side_overlay_frame.pack_propagate(False)
+        self.update()
         
-        self.top_overlay_frame.pack(fill=BOTH, expand="True")
-        self.center_overlay_frame.pack(fill=BOTH, expand="True")
+        self.top_overlay_width = self.top_overlay_frame.winfo_width()
+        self.top_overlay_height = self.top_overlay_frame.winfo_height()
+        
+        self.side_overlay_width = self.side_overlay_frame.winfo_width()
+        self.side_overlay_height = self.side_overlay_frame.winfo_height()
+        
+        self.center_overlay_width = self.center_overlay_frame.winfo_width()
+        self.center_overlay_height = self.center_overlay_frame.winfo_height()
